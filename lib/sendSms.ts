a07b4220bdd241c60037b0103862fcd0be1d19de
Trevor Keith <tk@tkeith.com>
2022-08-10 04:55:10 -0400

    misc fixes

--- lib/sendSms.ts
+++ lib/sendSms.ts
@@ -4,5 +4,5 @@ export async function sendSms(phoneNumber: string, text: string) {
   if (phoneNumber.endsWith('9999')) {
     return // test number, do not really send
   }
-  await plivoClient.messages.create(plivoFromNumber, phoneNumber, text);
+  console.log(await plivoClient.messages.create(plivoFromNumber, '1' + phoneNumber, text))
 }
